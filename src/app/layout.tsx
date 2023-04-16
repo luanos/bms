4ec68c3473049cb6df55e82856c9dea97f5e2b27
agelmarc <marcleonagel@gmail.com>
2023-04-16 16:37:13 +0200

    fix darm mode flickering

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,28 @@
 import { Metadata } from "next";
-
+import { Lato } from "next/font/google";
 import "~/styles/main.scss";
-import { ClientHTML } from "./html";
 
 export const metadata: Metadata = {
   title: "BMS Companion",
 };
 
+const darkModeScript = `if(window.matchMedia('(prefers-color-scheme: dark)').matches){document.documentElement.classList.add("dark-theme")}`;
+
+const lato = Lato({
+  weight: ["100", "300", "400", "700", "900"],
+  subsets: ["latin"],
+});
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <ClientHTML>
-      <body>{children}</body>
-    </ClientHTML>
+    <html className={lato.className}>
+      <body>
+        <script dangerouslySetInnerHTML={{ __html: darkModeScript }}></script>
+        {children}
+      </body>
+    </html>
   );
 }
